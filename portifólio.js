c598f8dc1995f53c00367f8ddec377a4a5734c9c
stenio sousa <steniosousaf@gmail.com>
2021-09-19 13:38:23 -0300

    fé

---
+++ portifólio.js
@@ -0,0 +1,4 @@
+function mostrar(){
+    var limonadadadada = document.getElementById("deseper").style.display = "inline-block";
+    var limonadadada = document.getElementById("revelar2").style.display = "none";
+}
