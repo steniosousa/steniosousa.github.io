47ebf5787093a53f95c0c09007051a9efdb26bdc
stenio <steniosousaf@gmail.com>
2021-10-14 20:02:26 -0300

    teste

--- portifólio.js
+++ portifólio.js
@@ -8,3 +8,25 @@ function voltar(){
 }
 
 
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+  target.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if(target.length) {
+  window.addEventListener('scroll', debounce(function() {
+    animeScroll();
+  }, 200));
+}
+
