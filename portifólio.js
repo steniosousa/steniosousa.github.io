5a65085fdbe602f0a2ba2709396b998f7fe8aa56
stenio <steniosousaf@gmail.com>
2021-10-14 20:40:38 -0300

    conserto

--- portifólio.js
+++ portifólio.js
@@ -7,25 +7,5 @@ function voltar(){
     var show = document.getElementById("projetos").style.display = "none";
 }
 
-const target = document.querySelectorAll('[data-anime]');
-const animationClass = 'animate';
 
-function animeScroll() {
-  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-  target.forEach(function(element) {
-    if((windowTop) > element.offsetTop) {
-      element.classList.add(animationClass);
-    } else {
-      element.classList.remove(animationClass);
-    }
-  })
-}
-
-animeScroll();
-
-if(target.length) {
-  window.addEventListener('scroll', debounce(function() {
-    animeScroll();
-  }, 200));
-}
 
